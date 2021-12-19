80953bd584536a3d6f39016a3974504e5d68c3ee
sapar6ek0v <razetka0309@gmail.com>
2021-12-19 17:26:28 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [poke, setPoke] = useState([])
+    const [rithg, setRithg] = useState({})
+    const [back, setBack] = useState([])
+    const [score, setScore] = useState('')
+    const [message, setMessage] = useState('')
+
+
+    useEffect(() => {
+        axios('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+            .then(({data}) => {
+                setPoke(data.pokemon)
+            })
+    }, [])
+
+    // const onChange = (e) => setFind(e.target.value.toLowerCase())
+
+    const startSearchPok = () => {
+        const random = Math.floor(Math.random() * 150)
+        setRithg(poke.find(it => it.id === random))
+        const numbers = [random, Math.floor(Math.random() * 150) + 1, Math.floor(Math.random() * 150) + 1, Math.floor(Math.random() * 150) + 1]
+        setBack(numbers.map(pokemon => {
+            return (
+                poke.find(it => it.id === pokemon)
+            )
+        }))
+
+    }
+
+    const giveAnswer = (id) => {
+        if (rithg.id === id) {
+            setScore(+score + 1)
+            setMessage("Вы выиграли")
+        } else {
+            setMessage("Вы проиграли")
+        }
+
+
+        startSearchPok()
+    }
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="app-block">
+                    <div className='sub-block'>
+                        <button onClick={startSearchPok} className='start-button'>start</button>
+                        <div className='text'>{score}</div>
+                        <img src={rithg.img} alt="pokemons" className='img'/>
+                        {
+                            back.map(it => {
+                                return <button onClick={() => giveAnswer(it.id)} className='button'>{it.name}</button>
+                            })
+                        }
+                        <div>{message}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+
+{/*<input onChange={onChange} type="text"/>*/
+}
+{/*<div className='row'>*/
+}
+
+{/*    /!*{poke.filter(pokemon => pokemon.name.toLowerCase().includes(find)).map(it => {*!/*/
+}
+{/*    /!*    return <div key={it.id} className='col4'>*!/*/
+}
+{/*    /!*        <img src={it.img} alt=""/>*!/*/
+}
+{/*    /!*        <div>{it.name}</div>*!/*/
+}
+{/*    /!*    </div>*!/*/
+}
+{/*    /!*})}*!/*/
+}
+{/*    */
+}
+{/*</div>*/
+}
